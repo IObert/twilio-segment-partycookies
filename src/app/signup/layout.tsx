640626a9e09b0308ffd35f7a583da732eaa59c4f
Nathaniel Okenwa <nokenwa@twilio.com>
2023-10-23 14:38:26 +0100

    styling added

---
+++ src/app/signup/layout.tsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+
+  
+    return (
+      <>
+       
+            {children}
+
+      </>
+    );
+  }
+  
