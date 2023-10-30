1ad13e59acf5956e1e70f7ed4e901e357849901b
Nathaniel Okenwa <nokenwa@twilio.com>
2023-10-30 17:18:10 +0000

    add faker to Nathaniel's demos

--- src/app/components/onLogin.js
+++ src/app/components/onLogin.js
@@ -1,8 +1,15 @@
 import { analytics } from "../../utils/analytics";
+import { faker } from "@faker-js/faker";
 //Only runs on successful login
 
 export default function onLogin({ email, phone }) {
   const traits = {
+    email,
+    phone,
+    username: faker.internet.userName(name),
+    avatar: faker.image.avatar(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
     email,
     phone,
     signedUpForRewards: true,
