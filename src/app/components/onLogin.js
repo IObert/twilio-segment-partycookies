b8e9b4a38ab9883ba0b375789d95c3f405b0f466
Marius Obert <obatzer@gmail.com>
2023-10-30 20:42:42 +0000

    add first and last name

--- src/app/components/onLogin.js
+++ src/app/components/onLogin.js
@@ -3,10 +3,16 @@ import { faker } from "@faker-js/faker";
 //Only runs on successful login
 
 export default function onLogin({ email, phone }) {
+
+  const name = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+  };
+
   const traits = {
     email,
     phone,
-    username: faker.internet.userName(),
+    username: faker.internet.userName(name),
     avatar: faker.image.avatar(),
     birthdate: faker.date.birthdate(),
     registeredAt: faker.date.past(),
