617a9b7a727daa64028fff007590a3e9f775bad8
Marius Obert <obatzer@gmail.com>
2023-11-28 13:12:39 +0100

    catch invalid num and redirect to error page

--- src/app/signup/signupAction.ts
+++ src/app/signup/signupAction.ts
@@ -52,13 +52,19 @@ export async function signupAction(formData: FormData) {
   });
   await analytics.closeAndFlush(); // flush because it'll run serverless when deployed
 
-  await sendMessage({
-    to: phone,
-    shortenUrls: true,
-    from: process.env.TWILIO_SENDER || "",
-    body: `Thanks for signing up for our dough-lightful news. For the sake of this demo, we assigned you a random name (${name.firstName}). 
-Don't forget to use these promo codes ${process.env.PROMO} when you create a new account ${process.env.UTM_LINK}`,
-  });
+  try {
+    await sendMessage({
+      to: phone,
+      shortenUrls: true,
+      from: process.env.TWILIO_SENDER || "",
+      body: `Thanks for signing up for our dough-lightful news. For the sake of this demo, we assigned you a random name (${name.firstName}). 
+      Don't forget to use these promo codes ${process.env.PROMO} when you create a new account ${process.env.UTM_LINK}`,
+    });
 
-  redirect(`.`); // Navigate to home page
+    redirect(`.`); // Navigate to home page
+  } catch (e) {
+    //@ts-ignore
+    console.error(e.message);
+    redirect(`./error`); // Navigate to error page
+  }
 }
